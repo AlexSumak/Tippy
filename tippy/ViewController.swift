009f69631006d27c3246bec36edd30d3483c7614
Alex Sumak <alex.sumak@Alex-Sumaks-MacBook-Air.local>
2016-12-17 21:26:29 -0800

    added new files releated to git

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var peopleControl: UISegmentedControl!
+    @IBOutlet weak var personPayLabel: UILabel!
+    @IBOutlet weak var taxLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +28,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true)
+    }
+  
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentage = [0.18, 0.2, 0.25]
+        let numPeople = [1,2,3,4]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let tax = Double(bill*0.08)
+        let total = bill + tip + tax
+        
+        
+        let personPay = total/Double(numPeople[peopleControl.selectedSegmentIndex])
+        
+    
+        personPayLabel.text = String(format: "$%.2f", personPay)
+        tipLabel.text = String(format: "$%.2f", tip)
+        taxLabel.text = String(format: "$%.2f", tax)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+ 
 }
 
